e160e1dc8a3cb3558046c612f07f96d031655a04
Naimur <m.naimur61@gmail.com>
2024-03-08 00:41:30 +0600

    Add Layout

---
+++ app/(root-layout)/home/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+	return (
+		<div className=" my-10 container mx-auto ">
+			<h1 className=" text-3xl font-serif font-semibold text-blue-600 text-center">This is home Page!</h1>
+		</div>
+	);
+};
+
+export default page;
