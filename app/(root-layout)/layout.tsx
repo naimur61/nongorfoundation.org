804021fb82df1e784d2f1113faf3462f84d71cfb
Naimur <m.naimur61@gmail.com>
2024-03-08 01:16:09 +0600

    navbar contact section

--- app/(root-layout)/layout.tsx
+++ app/(root-layout)/layout.tsx
@@ -7,10 +7,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<>
+		<div className="container mx-auto">
 			<Navbar />
 			{children}
 			<Footer />
-		</>
+		</div>
 	);
 }
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { PiClockCountdownBold } from "react-icons/pi";
+import { FaFacebookF, FaYoutube, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<section>
+			{/* Contact Information */}
+			<div className="bg-orange-600 p-5 text-white font-semibold">
+				<div className=" flex content-center items-center gap-16">
+					<div className=" flex content-center items-center gap-1">
+						<MdOutlineMarkEmailUnread />
+						<p>info@nongorfoundation.org</p>
+					</div>
+
+					<div className=" flex content-center items-center gap-1">
+						<PiClockCountdownBold />
+						<p>Mon - Sat: 08.00 am - 05:00</p>
+					</div>
+				</div>
+
+				<div className=" flex content-center items-center gap-1 ml-40">
+					<p>Pellentesque hendrerit turpis magna, non</p>
+					<FaFacebookF /> <FaYoutube /> <FaTwitter /> <FaInstagram />
+				</div>
+			</div>
+
+			<div>Header</div>
+		</section>
+	);
 };
 
 export default Navbar;
